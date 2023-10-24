baa0ee17b856bcf968533fddc090370b0ccd986f
Md Razwan Niam <razwanniam1@gmail.com>
2023-10-24 11:31:01 +0600

    array initialization and reversing

--- Array-W2/array_input_output.c
+++ Array-W2/array_input_output.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int ar[3];
-    ar[0] = 100;
-    ar[1] = 200;
-    printf("%d %d", ar[0], ar[1]);
-    return 0;
+    int ar[] = {10, 15, 20, 25, 30};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", ar[i]);
+    }
 }
