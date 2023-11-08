43baa15746c8767cf5ce17b0be47775dadd40a61
Md Razwan Niam <razwanniam1@gmail.com>
2023-11-08 21:43:37 +0600

    difference array problem solved

--- Difference_Array.c
+++ Difference_Array.c
@@ -2,37 +2,46 @@
 #include <string.h>
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int a[n], b[n];
-    for (int i = 0; i < n; i++)
+    int testcase;
+    scanf("%d", &testcase);
+    for (int i = 0; i < testcase; i++)
     {
-        scanf("%d", &a[i]);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        b[i] = a[i];
-    }
-    // for (int i = 0; i < n; i++)
-    // {
-    //     printf("%d ", b[i]);
-    // }
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = i + 1; j < n; j++)
+        int n;
+        scanf("%d", &n);
+        int a[n], b[n], c[n];
+        for (int i = 0; i < n; i++)
         {
-            if (a[i] > a[j])
+            scanf("%d", &a[i]);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            b[i] = a[i];
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
             {
-                int tmp = a[i];
-                a[i] = a[j];
-                a[j] = tmp;
+                if (b[i] > b[j])
+                {
+                    int tmp = b[i];
+                    b[i] = b[j];
+                    b[j] = tmp;
+                }
             }
         }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
+        for (int i = 0; i < n; i++)
+        {
+            c[i] = a[i] - b[i];
+            if (c[i] < 0)
+            {
+                c[i] = -c[i];
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", c[i]);
+        }
+        printf("\n");
     }
 
     return 0;
